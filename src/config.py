aa2bc47f18b42fad5d5a30aa8da39a0bf685fcae
Lucas Le Ray <lucas.a.leray@gmail.com>
2023-07-04 11:18:00 +0200

    Simplify features config

--- src/config.py
+++ src/config.py
@@ -1,14 +1,11 @@
-from types import SimpleNamespace
-
-
 class _Config:
     def __init__(self):
         self.test_size = .33
         self.target = 'trip_duration'
 
-        self.features = SimpleNamespace(
-            hours_bins=4,
-        )
+        self.features = {
+            "hours_bins": 4
+        }
 
 
 config = _Config()
