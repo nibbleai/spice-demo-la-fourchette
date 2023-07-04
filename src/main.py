43d2f41643b628f53034f90bef6f3f37fbe5b67c
Lucas Le Ray <lucas.a.leray@gmail.com>
2023-07-04 09:00:20 +0200

    Add model training

--- src/main.py
+++ src/main.py
@@ -1,4 +1,8 @@
+import logging
+
 from spice import Generator
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.metrics import mean_squared_error
 
 from src.data import get_dataset, get_target, train_test_split
 from src.features.registry import registry
@@ -12,6 +16,8 @@ def main():
     data, target = preprocess(data, target)
 
     train_data, test_data = train_test_split(data)
+    train_target = target.loc[train_data.index]
+    test_target = target.loc[test_data.index]
 
     feature_generator = Generator(
         registry,
@@ -24,8 +30,14 @@ def main():
             "manhattan_distance",
         ]
     )
-    train_features = feature_generator.fit_transform(train_data)
-    test_features = feature_generator.transform(test_data)
+    train_features = feature_generator.fit_transform(train_data).to_pandas()
+    test_features = feature_generator.transform(test_data).to_pandas()
+
+    model = RandomForestRegressor().fit(train_features, train_target)
+
+    predicted = model.predict(test_features)
+    m2_score = mean_squared_error(test_target, predicted)
+    logging.info(f"M2 score is {m2_score}")
 
 
 if __name__ == '__main__':
